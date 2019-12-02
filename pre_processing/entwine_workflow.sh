68b56e90e5dbae13363ec55441cfb1a608d76dca
Arno Timmer <arnot@leda.geodan.nl>
2019-12-02 16:40:45 +0100

    entwine config jsons to repo

--- pre_processing/entwine_workflow.sh
+++ pre_processing/entwine_workflow.sh
@@ -22,14 +22,23 @@ for i in $DATA_PATH/2019/amsterdam/* ; do
 done
 echo 'filtered 2019'
 
-entwine build -c $DATA_PATH/config-2018-cesium.json
+entwine build -c ./config-reproject.json \
+	-i $DATA_PATH/2018/filtered_las/ \
+	-o $DATA_PATH/2018/entwined-4978/
 echo 'tiled 2018 to epsg 4978'
-entwine build -c $DATA_PATH/config-2019-cesium.json
+
+entwine build -c ./config-reproject.json \
+        -i $DATA_PATH/2019/filtered_las/ \
+        -o $DATA_PATH/2019/entwined-4978/
 echo 'tiled 2019 to epsg 4978'
 
-entwine convert -i $DATA_PATH/2018/entwined-4978/ -o $DATA_PATH/2018/cesium/
+entwine convert \
+	-i $DATA_PATH/2018/entwined-4978/ \
+	-o $DATA_PATH/2018/cesium/
 echo 'converted 2018 to cesium';
-entwine convert -i $DATA_PATH/2019/entwined-4978/ -o $DATA_PATH/2019/cesium/
+entwine convert \
+	-i $DATA_PATH/2019/entwined-4978/ \
+	-o $DATA_PATH/2019/cesium/
 echo 'converted 2019 to cesium';
 
 
